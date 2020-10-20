bc55dd873ecfa19139bbb25bb882e70417fc86a3
Alejandro <agonzalezalonso@danielcastelao.org>
2020-10-20 11:22:02 +0200

    OlaMundo añadido.

---
+++ OlaMundo/src/com/dam1a/proyectoHelloMundo/Main.java
@@ -0,0 +1,10 @@
+package com.dam1a.proyectoHelloMundo;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Ola mundo!");
+    }
+}
